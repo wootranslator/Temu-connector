f4cff1e77f75be3a450f90a50b9f4de26d4df864
Javier Sánchez <sanchezdepedro85@gmail.com>
2026-01-08 17:42:38 +0100

    18.1.1.12

--- temu_odoo_integration/__manifest__.py
+++ temu_odoo_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Temu Odoo Integration',
-    'version': '18.1.1.10',
+    'version': '18.1.1.12',
     'category': 'Sales',
     'summary': 'Import sales orders from Temu to Odoo 18',
     'description': """
--- temu_odoo_integration/models/temu_connector.py
+++ temu_odoo_integration/models/temu_connector.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class TemuConnector(models.Model):
     _name = 'temu.connector'
